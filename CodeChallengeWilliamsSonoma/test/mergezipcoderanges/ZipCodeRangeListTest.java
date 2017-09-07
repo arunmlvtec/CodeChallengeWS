d14e13a0ff0b1db5f232770e3efa756ed60711b9
Arun Pokharna <arun.pokharna@gmail.com>
2017-09-07 02:28:15 -0700

    JUnit test case for ZipCodeRangeList class

--- CodeChallengeWilliamsSonoma/test/mergezipcoderanges/ZipCodeRangeListTest.java
+++ CodeChallengeWilliamsSonoma/test/mergezipcoderanges/ZipCodeRangeListTest.java
@@ -1,10 +1,15 @@
 /**
+ * Test Class: 	ZipCodeRangeListTest
+ * Description:	First Sort the input Zip Code Range and then merge the ranges.
  * 
  */
 package mergezipcoderanges;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,10 +17,31 @@ import org.junit.Test;
  *
  */
 public class ZipCodeRangeListTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	ZipCodeRangeList exec;
+	String expected = "resources/expected/output1.csv";
+	String input = "resources/input/input1.csv";
+	String actual = "resources/actual/actual1.csv";
+	ArrayList<ZipCodeRange> mergedExpecteds, mergedActuals;	
+	
+	@Before
+	public void before(){
+		exec = new ZipCodeRangeList();
+		mergedExpecteds = new ArrayList<ZipCodeRange>();
+		mergedExpecteds = exec.readInput(expected);
 	}
-
+	
+	@After
+	public void after() {
+		exec = null;
+		expected = null;
+	}
+	
+	@Test
+	public void testCase1() {
+		mergedActuals = new ArrayList<ZipCodeRange>();
+		mergedActuals = exec.executeMe(input, actual);
+		for (int i = 0; i < mergedActuals.size(); i++) {
+			Assert.assertEquals(mergedExpecteds.get(i).toString(), mergedActuals.get(i).toString());
+		}
+	}
 }
